616bf144e8e9b245be247eb20724d2b055328474
Dmitry M. Gavrovsky <dmitrygavrovsky@gmail.com>
2022-12-07 08:46:05 +0200

    key repeat speed script added

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -41,8 +41,7 @@ vim.o.guifont = "JetBrainsMono Nerd Font"
 
 local options = {
   backup = false, -- creates a backup file
---  clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-  clipboard = "", -- allows neovim to access the system clipboard
+  clipboard = "unnamedplus", -- allows neovim to access the system clipboard
   cmdheight = 1, -- more space in the neovim command line for displaying messages
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0, -- so that `` is visible in markdown files
